5ca2d034c58b28c0e0441a85df2a2f63b4afd1dc
Saul <saul.crespo.sc@gmail.com>
2023-04-17 09:04:01 +0200

    Este es el primer Commit que hago con GitHub.

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author saulc
+ *
+ */
+module Repositorios {
+}
---
+++ src/test/Ceinmark.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Ceinmark {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo, soy el CEINMARK.");
+	}
+}
