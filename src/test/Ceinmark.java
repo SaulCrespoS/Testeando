99003edb87ca5507d5ae0e73a844410381bd3553
Saul <saul.crespo.sc@gmail.com>
2023-04-17 09:17:37 +0200

    gregefdv

--- src/test/Ceinmark.java
+++ src/test/Ceinmark.java
@@ -2,6 +2,6 @@ package test;
 
 public class Ceinmark {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo, soy el CEINMARK.");
+		System.out.println("Hola mundo, soy el CEINMARK. ");
 	}
 }
